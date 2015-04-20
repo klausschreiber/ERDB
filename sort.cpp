f369630d91484ecf2c651d0d1d8f1c5e2990b49e
Sebastian Rückerl <sebastian@rueckerlmail.com>
2015-04-20 18:34:06 +0200

    some more error handling and structure. Added Error checking which might??? work as desired

--- sort.cpp
+++ sort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <cstring>
 #include <algorithm>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -32,15 +33,30 @@ void externalSort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
 
 }
 
+/// helper to cleanup fd and print error message if it failed 
+void inline cleanupFd(int fd) {
+    if (close(fd) < 0) {
+        std::cerr << "cannot close inputFile: " << strerror(errno) << std::endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 4) {
-        std::cout << "You need to specify all Parameters!" << std::endl;
-        std::cout << "try: sort <inputFile> <outputFile> <memoryBufferInMB>" << std::endl;
+        std::cout << "usage: sort <inputFile> <outputFile> <memoryBufferInMB>" << std::endl;
+        return -1;
+    }
+    else if (strcmp(argv[1], argv[2]) == 0){
+        std::cout << "inputFile and outputFile must not be the same file!" << std::endl;
+        return -1;
     }
     else {
+        //-------------------
+        //GENERATE INPUT DATA
+        //-------------------
         int fdInput, fdOutput;
         uint64_t memSize;
-        
+        uint64_t size;
+
         //open input File
         if ((fdInput = open(argv[1], O_RDONLY)) < 0) {
             std::cerr << "cannot open inputFile: " << strerror(errno) << std::endl;
@@ -50,9 +66,7 @@ int main(int argc, char* argv[]) {
         //open output File
         if ((fdOutput = open(argv[2], O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR|S_IWUSR)) < 0) {
             std::cerr << "cannot open outputFile: " << strerror(errno) << std::endl;
-            if (close(fdInput) < 0) {
-                std::cerr << "cannot close inputFile: " << strerror(errno) << std::endl;
-            }
+            cleanupFd(fdInput);
             return -1;
         }
 
@@ -60,20 +74,81 @@ int main(int argc, char* argv[]) {
         std::stringstream s(argv[3]);
         s >> memSize;
         memSize *= 1024*1024;
+        if (memSize == 0) {
+            std::cerr << "Invalid buffer size given (" << argv[3] << " Bytes)! Exiting!" << std::endl;
+            cleanupFd(fdInput);
+            cleanupFd(fdOutput);
+            return -1;
+        }
 
-        //do the test run
-        
-        std::cout << memSize << "Bytes used as buffer" << std::endl;
+        //get the number of elements within the infile
+        struct stat st;
+        if (fstat(fdInput, &st)) {
+            std::cerr << "Could not get stat of infile, thus sort cannot be executed!" << std::endl;
+            cleanupFd(fdInput);
+            cleanupFd(fdOutput);
+            return -1;
+        }
+        size = st.st_size / 8;
+
+        //be verbose (output the used input and output files and the buffer size)
+        std::cout << "Sorting " << argv[1] << " (containing "<< size << " elements) into " << argv[2] << " using a buffer of size " << memSize << "Bytes." << std::endl;
+
+        //----------------
+        //PERFORM TEST RUN
+        //----------------
+        //externalSort(fdInput, size, fdOutput, memSize);
 
         //cleanup (close file descriptors)
-        
-        if (close(fdInput) < 0) {
-            std::cerr << "cannot close inputFile: " << strerror(errno) << std::endl;
+        cleanupFd(fdInput);
+        cleanupFd(fdOutput);
+
+        //-------------------------
+        //CHECK SUCCESS OF TEST RUN
+        //-------------------------
+        int fdResult;
+
+        //open result file
+        if ((fdResult = open(argv[2], O_RDONLY)) < 0) {
+            std::cerr << "cannot open resultFile: " << strerror(errno) << std::endl;
+            return -1;
         }
-        if (close(fdOutput) < 0) {
-            std::cerr << "cannot close OutputFile: " << strerror(errno) << std::endl;
+
+        //first test: get stat of file to check if size is still the same
+        if (fstat(fdResult, &st)) {
+            std::cerr << "Could not get stat of resultFile!" << std::endl;
+            cleanupFd(fdResult);
+            return -1;
         }
+
+        if (size != (uint64_t) (st.st_size /8)) {
+            std::cout << "Size of result Elements (" << st.st_size / 8 << ") does not match original size(" << size << ")! Testing order anyways." << std::endl;
+        }
+
+        //next test: check if elements are in ascending order
+        uint64_t leftover = 0;
+        uint64_t * buffer = (uint64_t *) malloc(memSize);
+        ssize_t ret;
+
+        while ((ret = read(fdResult, buffer, memSize)) > 0) {
+            for (int i = 0; i < (ret/8); i++) {
+                //first element of current buffer
+                if (i == 0 && buffer[i] < leftover) {
+                    std::cout << "Error in result found: " << buffer[i] << " is following " << leftover << ", but it is smaller!" << std::endl;
+                }
+                else {
+                    if (buffer[i] < buffer[i-1]) {
+                        std::cout << "Error in result found: " << buffer[i] << " is following " << buffer[i-1] << ", but it is smaller!" << std::endl;
+                    }
+                    if (i == (ret/8 - 1)) {
+                        leftover = buffer[i];
+                    }
+                }
+            }
+        }
+
+        free(buffer);
+
     }
-    std::cout << "Program was run!" << argc << std::endl;
     return 0;
 }
